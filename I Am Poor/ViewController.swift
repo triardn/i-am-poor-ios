143e62776ab9c0ebc43e62f89de03fc96eb51174
Tri Ardini <tr.ardn@gmail.com>
2020-03-12 16:54:54 +0700

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Tri Ardini on 12/03/20.
+//  Copyright © 2020 Tri Ardini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
